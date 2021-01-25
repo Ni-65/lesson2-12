0b5d125b11b27bb0197f7ba316c7ba22419e1ba7
Nina <ni-65@mail.ru>
2021-01-25 21:17:27 +1000

    21

--- script-20.js
+++ script-20.js
@@ -1,21 +1,39 @@
 "use strict";
-function first(){
-	//do something
-	setTimeout(function(){
-		console.log(1);
-	}, 500);
-}
-function second(){
-	console.log(2);
-}
-first();
-second();
 
-function learnJS(lang, callback){
-	console.log(`Я учу: ${lang}`);
-	callback();
-}
-function done(){
-	console.log('Я прошел этот урок');
-}
-learnJS('JavaScript', done);
+const options = {
+	name: 'test',
+	width: 1024, 
+	height: 1024,
+	colors: {
+		border: 'black',
+		bg: 'red'
+	},
+	makeTest: function(){
+		console.log("Test");
+	}
+};
+options.makeTest();
+const{border, bg} = options.colors;
+console.log(border);
+
+// console.log(Object.keys(options).length); //!  надо научиться понимать
+
+// console.log(options["colors"]["border"]);
+
+// delete options.name;
+// console.log(options);
+
+
+// let counter = 0;
+// for (let key in options){
+// 	if (typeof(options[key]) === 'object'){
+// 		for (let i in options[key]){
+// 			console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
+// 			counter++;
+// 		}
+// 	}else{
+// 			console.log(`Свойства ${key} имеет значение ${options[key]}`);
+// 			counter++;
+// 	}
+// }
+// console.log(counter);
